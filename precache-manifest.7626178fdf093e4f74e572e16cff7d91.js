6d07f427a0301e5c162f88c2ca77c9323e10730f
Carina <carinalrobinson@gmail.com>
2020-06-18 16:49:36 +0200

    Updates

---
+++ precache-manifest.7626178fdf093e4f74e572e16cff7d91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f954347ce4d21acfbc6aba04daa5f5b",
+    "url": "/monsters_roledex/index.html"
+  },
+  {
+    "revision": "fe29a263959304519f5c",
+    "url": "/monsters_roledex/static/css/main.8a32aa5a.chunk.css"
+  },
+  {
+    "revision": "3045bc828fe84460bc3c",
+    "url": "/monsters_roledex/static/js/2.e667cf7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_roledex/static/js/2.e667cf7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe29a263959304519f5c",
+    "url": "/monsters_roledex/static/js/main.60d38ae9.chunk.js"
+  },
+  {
+    "revision": "018e339168fe9377177e",
+    "url": "/monsters_roledex/static/js/runtime-main.681014e4.js"
+  }
+]);
